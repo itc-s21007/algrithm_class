1e8fb1b5f71fa42dbf3c92406e88e3a452b5d1e3
s21007 <s221007@std.it-college.ac.jp>
2023-05-11 11:46:24 +0900

    Chapter2練習問題の提出

---
+++ 練習問題/Chapter2練習問題.py
@@ -0,0 +1,24 @@
+# 問２−１
+a = [10, -5, 0, 29, 6, 2, 77, 8]
+for n in range(8):
+    if a[n] % 2 == 0:
+        print("これは偶数です")
+    else:
+        print("これは奇数です")
+
+print("")
+
+# 問２−２
+for n in range(1,6):
+    print((3**n)-(2**n))
+
+print("")
+
+# 問２−３
+print("☓")
+print("☓")
+print("○")
+print("☓")
+print("☓")
+print("○")
+
